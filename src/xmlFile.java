ba049d963b1c544cbb74d4d15e0997146e953d42
MirunaLM <46753626+MirunaLM@users.noreply.github.com>
2019-05-17 15:36:22 +0300

    Update xmlFile.java

--- src/xmlFile.java
+++ src/xmlFile.java
@@ -6,10 +6,14 @@ import java.util.Scanner;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
-import org.jdom2.JDOMException;
+import org.jdom2.JDOMException; 
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
+
+/*
+ * The xmlFile class takes care of the XML file which contains questions and answers. It can add new elements and it can update when a change takes place.
+ */
 
 public class xmlFile {
 	private Element root;
@@ -24,15 +28,29 @@ public class xmlFile {
 		root = document.getRootElement();
 	}
 
+	/*
+	 * This method adds new tags in the XML file
+	 * 
+	 * @param humanText = The question addressed by the user
+	 * 
+	 * @param answer = the text which is added as answer for the question
+	 */
 	public void addElement(String humanText, String answer) {
 		newElement = root;
+		// Here a new "question" tag is created
 		question = new Element("question").setText(humanText);
 		newElement.addContent(question);
 
 		newChildElement = question;
+		// Here the new tag will receive an childTag "answer"
 		answer2 = new Element("answer").setText(answer);
 		newChildElement.addContent(answer2);
 	}
+
+	/*
+	 * This method adds new tags in the XML file, but here the answer is introduced
+	 * in the console
+	 */
 
 	public void addElement(String humanText) {
 		newElement = root;
@@ -45,10 +63,13 @@ public class xmlFile {
 		String newWord = sc.nextLine();
 
 		newChildElement = question;
-		answare = new Element("answer").setText(newWord);
-		newChildElement.addContent(answare);
+		answer2 = new Element("answer").setText(newWord);
+		newChildElement.addContent(answer2);
 	}
 
+	/*
+	 * This method updates the XML file
+	 */
 	public void xmlUpdate(Document document, File file) {
 		XMLOutputter xmlOutput = new XMLOutputter();
 		xmlOutput.setFormat(Format.getRawFormat());
