c4616f5d409efdf4e6b78f815ada3d8da130bd1c
MirunaLM <46753626+MirunaLM@users.noreply.github.com>
2019-05-17 14:36:15 +0300

    Update ChatBot.java

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -8,51 +8,81 @@ import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
+/*
+ * The ChatBot class takes care of the chat's actions.It can replay to questions and can offer with precision weather info.
+ * It is capable to recognize questions about weather by certain words
+ */
+
 public class ChatBot {
 
-	private Element raspuns;
+	private Element answer;
 	private String xmlText;
 
-	public String searchRaspuns(xmlFile xmlFile, String humanText, JTextArea jtextarea) {
+	/*
+	 * This method seeks the question addressed by the user in the XML file.
+	 * 
+	 * @return the extracted answer from the ChildTag of the specific question or
+	 * NULL if the question does not exist in the XML file
+	 */
+	public String searchAnswer(xmlFile xmlFile, String humanText, JTextArea jtextarea) {
 		for (int temp = 0; temp < xmlFile.getChatList().size(); temp++) {
 			Element message = xmlFile.getChatList().get(temp);
 			xmlText = message.getText();
 			List<Element> listaRaspunsuri = message.getChildren();
-			raspuns = listaRaspunsuri.get(0);
-			if (humanText.equals(xmlText))
-				return raspuns.getText();
+			answer = listaRaspunsuri.get(0);
+			if (humanText.equals(xmlText)) {
+				return answer.getText();
+			}
 		}
-		jtextarea.append(
-				"ChatBot : Nu stiu inca raspunsul la intrebarea ta, insa ma poti face mai destept \n daca scri tu unul scrie in consola \n");
 		return null;
 	}
 
-	public void printRaspuns(JTextArea jtextarea, String raspunss) {
-		jtextarea.append("ChatBot : " + raspunss + "\n");
-
+	public void printAnswer(JTextArea jtextarea, String answer) {
+		jtextarea.append("ChatBot : " + answer + "\n");
 	}
 
-	public void raspunsuriVreme(JTextArea jtextarea, String question, Vreme vreme, SAXBuilder saxBuilder, File file)
+	/*
+	 * This method divides the question stated by the user in conformity with the
+	 * character " " This check if in the question itself certain words exist(eg.
+	 * acum)
+	 * 
+	 * @param humanText = The question addressed by the user
+	 */
+
+	public boolean checkWeather(String humanText) {
+		for (String word : humanText.split(" ")) {
+			if ((word.equals("acum") || word.equals("temperatura") || word.equals("maine?") || word.equals("maine")
+					|| word.equals("rasaritul") || word.equals("acum?") || word.equals("azi") || word.equals("acum")
+					|| word.equals("azi?"))) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/*
+	 * If the checkWeather function is TRUE, then it executes the function which
+	 * shows the weather during a certain day in conformity with which certain
+	 * specific words were found in the structure of the question
+	 */
+
+	public void answerWeather(JTextArea jtextarea, Weather weather, SAXBuilder saxBuilder, File file, String humanText)
 			throws JDOMException, IOException {
-		switch (question) {
-		case "cum este vremea acum?":
-			vreme.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "name", jtextarea, 0, 0);
-			vreme.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "localtime", jtextarea, 0, 0);
-			vreme.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "text", jtextarea, 0, 1);
-			vreme.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "temp_c", jtextarea, 0, 1);
-			break;
-		case "cum este vremea maine?":
-			vreme.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "name", jtextarea, 0, 0);
-			vreme.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "date", jtextarea, 0, 3);
-			vreme.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "text", jtextarea, 0, 3);
-			vreme.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "maxtemp_c", jtextarea, 0, 3);
-			break;
-		case "care este temperatura maxima maine?":
-			vreme.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "maxtemp_c", jtextarea, 0, 3);
-			break;
-		case "care este temperatura maxima in 2 zile?":
-			vreme.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "maxtemp_c", jtextarea, 0, 4);
-			break;
+
+		for (String word : humanText.split(" ")) {
+			if ((word.equals("acum") || word.equals("azi") || word.equals("ploua?") || word.equals("azi?"))) {
+				weather.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "name", jtextarea, 0, 0);
+				weather.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "localtime", jtextarea, 0, 0);
+				weather.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "text", jtextarea, 0, 1);
+				weather.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "temp_c", jtextarea, 0, 1);
+			}
+			if ((word.equals("maine") || word.equals("maine?"))) {
+				weather.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "name", jtextarea, 0, 0);
+				weather.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "date", jtextarea, 0, 3);
+				weather.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "text", jtextarea, 0, 3);
+				weather.searchXML(saxBuilder.build(file).getRootElement().getChildren(), "maxtemp_c", jtextarea, 0, 3);
+			}
 		}
 	}
 
